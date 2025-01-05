b2d97742e9bf8168c671b686d5545d3ff52fe040
Saksham <saksham3112853@gmail.com>
2025-01-05 22:53:06 +0530

    ff

--- server/index.js
+++ server/index.js
@@ -25,7 +25,9 @@ const imagekit=new ImageKit({
     publicKey:process.env.VITE_IMAGE_KIT_PUBLIC_KEY,
     privateKey:process.env.VITE_IMAGE_KIT_PRIVATE_KEY
 })
-
+app.get('/api/', (req, res) => {
+    res.json({ message: 'API Version 1' });
+  });
 app.get('/api/upload',(req,res)=>{
     const result=imagekit.getAuthenticationParameters()
     res.send(result)
